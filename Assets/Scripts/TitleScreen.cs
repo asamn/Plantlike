db0c4b5b250f42bbde4adefb832be8992f0cd07c
adam sam <aui80180@gmail.com>
2024-07-27 16:09:49 -0400

    get things ready for world size options

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleScreen : MonoBehaviour
 {
     private GameObject flower;
-
+    [SerializeField] private GameObject startButton, quitButton;
+    [SerializeField] private GameObject worldOptions;
     public void StartGame()
     {
         print("START");
         SceneManager.LoadScene("PlantGame");
 
-    }
+    }    
+    public void ShowWorldOptions()
+    {
+        startButton.SetActive(false);
+        quitButton.SetActive(false);
 
+        worldOptions.SetActive(true);
+    }
     public void ExitGame()
     {
         print("Quitting");
